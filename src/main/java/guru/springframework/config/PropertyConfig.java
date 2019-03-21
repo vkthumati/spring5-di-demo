0d66e5adc5345922beb1b85a9b141edd3f250305
vkthumati <info2vasanth@gmail.com>
2019-03-21 17:34:50 -0500

    Added application yml and properties files with profiles

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -7,11 +7,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
+/*@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+    }
+)*/
 public class PropertyConfig {
     @Autowired
     Environment environment;
@@ -38,8 +44,8 @@ public class PropertyConfig {
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setDbUrl(dbUrl);
-        //fakeDataSource.setUserName(user);
-        fakeDataSource.setUserName(environment.getProperty("USERNAME"));
+        fakeDataSource.setUserName(user);
+        //fakeDataSource.setUserName(environment.getProperty("USERNAME"));
         fakeDataSource.setPwd(pwd);
         return fakeDataSource;
     }
